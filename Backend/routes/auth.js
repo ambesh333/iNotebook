aef85b158f7a571c191a030690b510b9b32528ad
Ambesh <ambeshgaunker@gmail.com>
2022-01-07 17:42:51 +0530

    updating notes, deleting notes,~Frontend

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const  bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var fetchuser = require('../middleware/fetchuser');
+
 
 const JWT_SECRET = 'AmbeshGaunker';
 
-//create a user using:post "/api/auth/createuser" . No login required
+//ROUTE 1:create a user using:post "/api/auth/createuser" . No login required
 router.post(
   "/createusers",
   [
@@ -43,7 +45,7 @@ router.post(
 // create a new user
       user = await User.create({
         name: req.body.name,
-       
+        
         password: secPass,
         email: req.body.email,
       });
@@ -64,5 +66,71 @@ router.post(
     }
   }
 );
+
+//ROUTE 2:authenticate a user using:post "/api/auth/login" . No login required
+router.post(
+  "/login",
+  [ 
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Cannot be blanked").exists(),
+  ],
+  async (req, res) => {
+   
+      //if there are errors ,return bad request and the errors
+
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const {email,password}= req.body;
+      try {
+        let user= await  User.findOne({email});
+        if(!user){
+          return res.status(400).json({error: "Please try to login with correct credentials"});
+        }
+        const passwordCompare = await bcrypt.compare(password,user.password)
+        if(!passwordCompare){
+          return res.status(400).json({error: "Please try to login with correct credentials"});
+        }
+       
+
+        const data = {
+          user:{
+            id: user.id
+          }
+        }
+
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken})
+
+
+      }  catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server  error ");
+      }
+
+
+  })
+
+  //ROUTE 3:GEt loggedIn user details using:post "/api/auth/getuser" .  login required
+  router.post(
+    "/getuser",fetchuser,
+  
+    async (req, res) => {
+
+try {
+    userId= req.user.id;
+   const user = await User.findById(userId).select("-password")
+   res.send(user)
+  
+  
+} catch (error) {
+  console.error(error.message);
+  res.status(500).send("Internal server  error ");
+}
+
+})
+
 
 module.exports = router;
---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+export const Home = () => {
+    return (
+        <div>
+           <h1>This is INotebook</h1>
+        </div>
+    )
+}
+
+export default Home
